f05bcc6e28bfca235d049435b0eae0fb9dd1ef17
Local Piper <artsor53@gmail.com>
2024-10-02 12:49:38 +0300

    chore: added constructors for models

--- lab1/src/main/java/com/example/lab1/models/City.java
+++ lab1/src/main/java/com/example/lab1/models/City.java
@@ -2,6 +2,11 @@ package com.example.lab1.models;
 
 import java.time.ZonedDateTime;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class City {
     private int id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
@@ -16,4 +21,15 @@ public class City {
     private Climate climate; //Поле не может быть null
     private Government government; //Поле может быть null
     private Human governor; //Поле может быть null
+    public City(String name, Coordinates coordinates, double area, int population, Climate climate, Government government, Human human) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = java.time.ZonedDateTime.now();
+        this.area = area;
+        this.population = population;
+        this.climate = climate;
+        this.government = government;
+        this.governor = human;
+    }
 }
+
