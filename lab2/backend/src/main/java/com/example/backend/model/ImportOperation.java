8bb89b0a67ad8a98ba35474597bb6209208cf2ad
Local Piper <artsor53@gmail.com>
2024-11-29 23:17:34 +0300

    feat: importop model

---
+++ lab2/backend/src/main/java/com/example/backend/model/ImportOperation.java
@@ -0,0 +1,37 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="import_operations")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImportOperation {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private OperationStatus status;
+
+    @Column(nullable=false)
+    @NotBlank
+    private String username;
+
+    @Column
+    @Min(0)
+    private Integer objectCount;
+}
